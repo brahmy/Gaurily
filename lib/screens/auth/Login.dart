3f0db98198af5dabc53fae9bb995e03280d0e5b1
VamsiBoggala <vamsiboggala@gamil.com>
2021-10-09 19:11:33 +0530

    Flow Update

--- lib/screens/auth/Login.dart
+++ lib/screens/auth/Login.dart
@@ -1,48 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:gaurily/screens/HomeCategoryTabs.dart';
-
+import 'package:gaurily/screens/grid_screen.dart';
 
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(50.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Gaurily',
-                style: Theme.of(context).textTheme.headline1,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Username',
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.all(50.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Gaurily',
+                  style: Theme.of(context).textTheme.headline1,
                 ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Password',
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Username',
+                  ),
                 ),
-                obscureText: true,
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              ElevatedButton(
-                child: Text('LOGIN'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomeCategoryTabs()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.yellow,
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                  ),
+                  obscureText: true,
                 ),
-              )
-            ],
+                SizedBox(
+                  height: 24,
+                ),
+                ElevatedButton(
+                  child: Text('LOGIN'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => GridViewScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.yellow,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/screens/grid_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:gaurily/screens/grid_screen_names.dart';
+
+class GridViewScreen extends StatelessWidget {
+  List<String> _images = [
+    "https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1541182388496-ac92a3230e4c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8OHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1578298245355-3c600a9a13d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1535268244390-8b989b92d2bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1562620662-49e245726f8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NzZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1530047198515-516ff90fc4d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8ODN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1521567097888-2c5fc40a8660?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1557864691-fa78b6d486c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1593380090147-a2192b72a9ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1509675129439-5c3e857f52e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NTd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: StaggeredGridView.countBuilder(
+          crossAxisCount: 2,
+          itemCount: _images.length,
+          itemBuilder: (context, index) => GestureDetector(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(_images[index]),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => GridScreenNames(),
+                ),
+              );
+            },
+          ),
+          staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+          mainAxisSpacing: 8.0,
+          crossAxisSpacing: 8.0,
+        ),
+      ),
+    );
+  }
+}
